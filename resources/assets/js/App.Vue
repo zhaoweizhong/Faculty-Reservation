d4fea0c68f1d0cab4acc5946bf24cf842a70c626
Zhaowei Zhong <zhong98zhaowei@gmail.com>
2018-09-12 14:03:58 +0800

    Laravel Presets Setted

---
+++ resources/assets/js/App.Vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <example></example>
+    <el-button icon="edit">Hello Element</el-button>
+  </div>
+</template>
+<script>
+  import Example from './components/Example.vue';
+  export default {
+    name: 'app',
+    components: {
+      Example
+    }
+  };
+</script>
