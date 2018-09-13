bd55806696c3ddc04b6063c624e6597d2b969c4d
Zhaowei Zhong <zhong98zhaowei@gmail.com>
2018-09-13 14:02:14 +0800

    Antd Import Finished

---
+++ resources/assets/js/store/modules/setting.js
@@ -0,0 +1,26 @@
+export default {
+  namespaced: true,
+  state: {
+    isMobile: false,
+    theme: 'dark',
+    layout: 'side',
+    systemName: 'Vue Antd Admin',
+    copyright: '2018 ICZER 工作室出品',
+    footerLinks: [
+      {link: 'https://pro.ant.design', name: 'Pro首页'},
+      {link: 'https://github.com/iczer/vue-antd-admin', icon: 'github'},
+      {link: 'https://ant.design', name: 'Ant Design'}
+    ]
+  },
+  mutations: {
+    setDevice (state, isMobile) {
+      state.isMobile = isMobile
+    },
+    setTheme (state, theme) {
+      state.theme = theme
+    },
+    setLayout (state, layout) {
+      state.layout = layout
+    }
+  }
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>测试</title>
-</head>
-<body>
-  <div id="app"></div>
-
-  <script src="{{ mix('js/app.js') }}"></script>
-</body>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>vue-antd-admin</title>
+  </head>
+  <body>
+    <div id="app"></div>
+    <!-- built files will be auto injected -->
+    <script src="{{ mix('js/main.js') }}"></script>
+  </body>
 </html>
