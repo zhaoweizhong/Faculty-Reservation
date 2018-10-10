1dde1a23871e27ff2e3060e645c978e5ac7e2b19
Zhaowei Zhong <zhong98zhaowei@gmail.com>
2018-10-10 14:14:10 +0800

    Some Patterns Updated

--- resources/assets/js/pages/user/Profile.vue
+++ resources/assets/js/pages/user/Profile.vue
@@ -63,7 +63,7 @@
             <detail-list-item term="院系">{{ currUser.department }}</detail-list-item>
             <detail-list-item term="专业">{{ currUser.major }}</detail-list-item>
             <detail-list-item term="GPA">{{ currUser.gpa }}</detail-list-item>
-            <detail-list-item term="感兴趣的方向">{{ currUser.interested_fields }}</detail-list-item>
+            <detail-list-item term="兴趣方向">{{ currUser.interested_fields }}</detail-list-item>
         </detail-list>
         <a-divider style="margin-bottom: 32px" />
         <div class="title">个人介绍</div>
@@ -72,201 +72,208 @@
 </template>
 
 <script>
-import ACard from 'ant-design-vue/es/card/Card'
-import AButton from 'ant-design-vue/es/button/button'
-import ATag from 'ant-design-vue/es/tag/Tag'
-import ATooltip from 'ant-design-vue/es/tooltip/Tooltip'
-import AAvatar from 'ant-design-vue/es/avatar/Avatar'
-import AIcon from 'ant-design-vue/es/icon/icon'
-import DetailList from '../../components/tool/DetailList'
-import ADivider from 'ant-design-vue/es/divider/index'
-import PageLayout from '../../layouts/PageLayout'
-import AvatarCropper from "vue-avatar-cropper"
+import ACard from "ant-design-vue/es/card/Card";
+import AButton from "ant-design-vue/es/button/button";
+import ATag from "ant-design-vue/es/tag/Tag";
+import ATooltip from "ant-design-vue/es/tooltip/Tooltip";
+import AAvatar from "ant-design-vue/es/avatar/Avatar";
+import AIcon from "ant-design-vue/es/icon/icon";
+import DetailList from "../../components/tool/DetailList";
+import ADivider from "ant-design-vue/es/divider/index";
+import PageLayout from "../../layouts/PageLayout";
+import AvatarCropper from "vue-avatar-cropper";
 
-const DetailListItem = DetailList.Item
+const DetailListItem = DetailList.Item;
 
 export default {
-    name: 'BasicDetail',
-    components: {
-        PageLayout,
-        ADivider,
-        DetailListItem,
-        DetailList,
-        AButton,
-        AAvatar,
-        ATooltip,
-        ACard,
-        AIcon,
-        ATag,
-        AvatarCropper
-    },
-    computed: {
-      currUser () {
-        return this.$store.state.account.user
-      }
-    },
-    outputOptions: {
-        type: Object,
-        default() {
-          return {
-            width: 512,
-            height: 512
-          }
-        }
-      },
-    mounted() {
-        $("#avatar").hover(function(){
-            $("#mask").removeClass("mask-hidden");
-        },function(){
-            $("#mask").addClass("mask-hidden");
-        });
-    },
-    methods: {
-        handleUpload(result) {
-            result.getCroppedCanvas(this.outputOptions).toBlob((blob) => {
-                var formData = new FormData()
-                formData.append("image", blob)
-                let config = {
-                    headers:{'Content-Type':'multipart/form-data'}
-                };
-                this.$axios.post('/api/images', formData, config)
-                .then((resp) => {
-                    let res = resp.data;
-                    console.log('resp ' + resp)
-                    if (res.status_code == 201) {
-                        this.$store.state.account.user.avatar_url = res.url;
-                        this.$axios.patch('/api/user', {
-                            avatar_url: res.url
-                        }).then((response) => {
-                            console.log('response ' + response)
-                            let result = response.data
-                            if (response.status == 201) {
-                                this.$message.success('头像设置成功')
-                            }
-                        }).catch((error) => {
-                            this.$message.error('头像设置失败');
-                            console.log('User Patch Error: ' + error.response.data.message);
-                        })
-                    } else {
-                        this.$message.error('头像设置失败');
-                        console.log('Upload Error: ' + JSON.stringify(res));
-                    }
-                }).catch((err) => {
-                    this.$message.error('头像设置失败');
-                    console.log('Upload Error: ' + JSON.stringify(err));
-                });
-            })
-        }
+  name: "BasicDetail",
+  components: {
+    PageLayout,
+    ADivider,
+    DetailListItem,
+    DetailList,
+    AButton,
+    AAvatar,
+    ATooltip,
+    ACard,
+    AIcon,
+    ATag,
+    AvatarCropper
+  },
+  computed: {
+    currUser() {
+      return this.$store.state.account.user;
     }
-}
-
-
-
+  },
+  outputOptions: {
+    type: Object,
+    default() {
+      return {
+        width: 512,
+        height: 512
+      };
+    }
+  },
+  mounted() {
+    $("#avatar").hover(
+      function() {
+        $("#mask").removeClass("mask-hidden");
+      },
+      function() {
+        $("#mask").addClass("mask-hidden");
+      }
+    );
+  },
+  methods: {
+    handleUpload(result) {
+      result.getCroppedCanvas(this.outputOptions).toBlob(blob => {
+        var formData = new FormData();
+        formData.append("image", blob);
+        let config = {
+          headers: { "Content-Type": "multipart/form-data" }
+        };
+        this.$axios
+          .post("/api/images", formData, config)
+          .then(resp => {
+            let res = resp.data;
+            console.log("resp " + resp);
+            if (res.status_code == 201) {
+              this.$store.state.account.user.avatar_url = res.url;
+              this.$axios
+                .patch("/api/user", {
+                  avatar_url: res.url
+                })
+                .then(response => {
+                  console.log("response " + response);
+                  let result = response.data;
+                  if (response.status == 201) {
+                    this.$message.success("头像设置成功");
+                  }
+                })
+                .catch(error => {
+                  this.$message.error("头像设置失败");
+                  console.log(
+                    "User Patch Error: " + error.response.data.message
+                  );
+                });
+            } else {
+              this.$message.error("头像设置失败");
+              console.log("Upload Error: " + JSON.stringify(res));
+            }
+          })
+          .catch(err => {
+            this.$message.error("头像设置失败");
+            console.log("Upload Error: " + JSON.stringify(err));
+          });
+      });
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
 .title {
-    color: rgba(0,0,0,.85);
-    font-size: 16px;
-    font-weight: 500;
-    margin-bottom: 16px;
+  color: rgba(0, 0, 0, 0.85);
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 16px;
 }
 
 .info {
-    position: relative;
-    min-height: 140px;
+  position: relative;
+  min-height: 140px;
 }
 
 .avatar {
-    float: left;
-    .avatar-wrapper {
-        cursor: pointer;
-    }
-    .mask {
-        position: absolute;
-        top: 0;
-        right: 0;
-        bottom: 0;
-        left: 0;
-        z-index: 1;
-        width: 140px;
-        transition: opacity .2s ease-in;
-    }
-    .mask-hidden {
-        pointer-events: none;
-        opacity: 0;
-    }
-    .mask-inner {
-        z-index: 4;
-        border-radius: 4px;
-        position: absolute;
-        z-index: -1;
-        width: 100%;
-        height: 100%;
-        opacity: .4;
-        background: #1a1a1a;
-        box-sizing: border-box;
-    }
-    .mask-content {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        z-index: 5;
-        color: #fff;
-        text-align: center;
-        -webkit-transform: translate(-50%,-50%);
-        transform: translate(-50%,-50%);
-    }
-    .mask-text {
-        white-space: nowrap;
-    }
+  float: left;
+  .avatar-wrapper {
+    cursor: pointer;
+  }
+  .mask {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1;
+    width: 140px;
+    transition: opacity 0.2s ease-in;
+  }
+  .mask-hidden {
+    pointer-events: none;
+    opacity: 0;
+  }
+  .mask-inner {
+    z-index: 4;
+    border-radius: 4px;
+    position: absolute;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    opacity: 0.4;
+    background: #1a1a1a;
+    box-sizing: border-box;
+  }
+  .mask-content {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    z-index: 5;
+    color: #fff;
+    text-align: center;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  }
+  .mask-text {
+    white-space: nowrap;
+  }
 }
 
 .info-content {
-    padding-top: 40px;
-    padding-left: 180px;
+  padding-top: 40px;
+  padding-left: 180px;
 }
 
 .name {
-    margin-bottom: 13px;
-    span {
-        color: #000;
-        font-size: 30px;
-        font-weight: 600;
-        font-synthesis: style;
-        line-height: 30px;
-    }
+  margin-bottom: 13px;
+  span {
+    color: #000;
+    font-size: 30px;
+    font-weight: 600;
+    font-synthesis: style;
+    line-height: 30px;
+  }
 
-    .ant-tag {
-        font-size: 13px;
-        margin-left: 10px;
-        height: 23px;
-        vertical-align: super;
-    }
+  .ant-tag {
+    font-size: 13px;
+    margin-left: 10px;
+    height: 23px;
+    vertical-align: super;
+  }
 }
 
 .detail > div > div {
-    margin-bottom: 4px;
-    font-size: 15px;
-    i {
-        margin-right: 5px;
-    }
-    .divider {
-        display: inline-block;
-        width: 1px;
-        height: 11px;
-        margin: 0 8px;
-        background: #ebebeb;
-    }
+  margin-bottom: 4px;
+  font-size: 15px;
+  i {
+    margin-right: 5px;
+  }
+  .divider {
+    display: inline-block;
+    width: 1px;
+    height: 11px;
+    margin: 0 8px;
+    background: #ebebeb;
+  }
 }
 
 .edit {
-    position: absolute;
-    float: right;
-    top: 0;
-    right: 0;
-    a > button {
-        height: 37px;
-    }
+  position: absolute;
+  float: right;
+  top: 0;
+  right: 0;
+  a > button {
+    height: 37px;
+  }
 }
 </style>
--- resources/assets/js/pages/user/Settings.vue
+++ resources/assets/js/pages/user/Settings.vue
@@ -13,9 +13,9 @@
                     mode="inline"
                     @click="handleClick"
                 >
-                    <a-menu-item key="1">个人资料</a-menu-item>
-                    <a-menu-item key="2">安全设置</a-menu-item>
-                    <a-menu-item key="3">其他设置</a-menu-item>
+                    <a-menu-item key="1"><a-icon type="profile" />个人资料</a-menu-item>
+                    <a-menu-item key="2"><a-icon type="lock" />安全设置</a-menu-item>
+                    <a-menu-item key="3"><a-icon type="setting" />其他设置</a-menu-item>
                 </a-menu>
             </div>
 
@@ -69,17 +69,28 @@
                             <a-form-item
                                 :labelCol="{ span: 8 }"
                                 :wrapperCol="{ span: 24 }"
-                                label='感兴趣的方向'
+                                label='兴趣方向'
                                 :colon='false'
                                 fieldDecoratorId="interested_fields"
-                                :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入感兴趣的方向' }]}"
+                                :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入兴趣方向' }]}"
                             >
-                                <a-textarea placeholder='感兴趣的方向...' :value='currUser.interested_fields' id='interested_fields' :autosize="{ minRows: 2 }"/>
+                                <a-textarea placeholder='兴趣方向...' :value='currUser.interested_fields' id='interested_fields' :autosize="{ minRows: 2 }"/>
                             </a-form-item>
                         </a-form>
                     </div>
                     <div class="right ant-col-md-16">
-
+                        <a-form>
+                            <a-form-item
+                                :labelCol="{ span: 3 }"
+                                :wrapperCol="{ span: 24 }"
+                                label='个人介绍'
+                                :colon='false'
+                                fieldDecoratorId="intro"
+                                :fieldDecoratorOptions="{rules: [{ required: true, message: '请输入个人介绍' }]}"
+                            >
+                                <a-textarea placeholder='个人介绍...' :value='currUser.intro' id='intro' :autosize="{ minRows: 20 }"/>
+                            </a-form-item>
+                        </a-form>
                     </div>
                 </div>
             </div>
@@ -88,7 +99,7 @@
                     <span>安全设置</span>
                 </div>
             </div>
-            <div v-else="this.current == '3'" class="col-right">
+            <div v-else class="col-right">
                 <div class="title">
                     <span>其他设置</span>
                 </div>
@@ -99,110 +110,114 @@
 </template>
 
 <script>
-import ACard from 'ant-design-vue/es/card/Card'
-import AForm from 'ant-design-vue/es/form/Form'
-import AFormItem from 'ant-design-vue/es/form/FormItem'
-import AInput from 'ant-design-vue/es/input/Input'
-import ATextarea from 'ant-design-vue/es/input/TextArea'
-import AButton from 'ant-design-vue/es/button/button'
-import ATag from 'ant-design-vue/es/tag/Tag'
-import ATooltip from 'ant-design-vue/es/tooltip/Tooltip'
-import AIcon from 'ant-design-vue/es/icon/icon'
-import DetailList from '../../components/tool/DetailList'
-import ADivider from 'ant-design-vue/es/divider/index'
-import PageLayout from '../../layouts/PageLayout'
-import AMenu from 'ant-design-vue/es/menu/index'
+import ACard from "ant-design-vue/es/card/Card";
+import AForm from "ant-design-vue/es/form/Form";
+import AFormItem from "ant-design-vue/es/form/FormItem";
+import AInput from "ant-design-vue/es/input/Input";
+import ATextarea from "ant-design-vue/es/input/TextArea";
+import AButton from "ant-design-vue/es/button/button";
+import ATag from "ant-design-vue/es/tag/Tag";
+import ATooltip from "ant-design-vue/es/tooltip/Tooltip";
+import AIcon from "ant-design-vue/es/icon/icon";
+import DetailList from "../../components/tool/DetailList";
+import ADivider from "ant-design-vue/es/divider/index";
+import PageLayout from "../../layouts/PageLayout";
+import AMenu from "ant-design-vue/es/menu/index";
 
-const AMenuItem = AMenu.Item
-const DetailListItem = DetailList.Item
+const AMenuItem = AMenu.Item;
+const DetailListItem = DetailList.Item;
 
 export default {
-    name: 'BasicDetail',
-    components: {
-        AMenu,
-        AMenuItem,
-        PageLayout,
-        ADivider,
-        DetailListItem,
-        DetailList,
-        AButton,
-        ATooltip,
-        ACard,
-        AInput,
-        ATextarea,
-        AFormItem,
-        AForm,
-        AIcon,
-        ATag
-    },
-    data() {
-        return {
-            current: '1'
-        }
-    },
-    computed: {
-        currUser() {
-            return this.$store.state.account.user
-        }
-    },
-    methods: {
-        handleClick(e) {
-            this.current = e.key
-        }
+  name: "BasicDetail",
+  components: {
+    AMenu,
+    AMenuItem,
+    PageLayout,
+    ADivider,
+    DetailListItem,
+    DetailList,
+    AButton,
+    ATooltip,
+    ACard,
+    AInput,
+    ATextarea,
+    AFormItem,
+    AForm,
+    AIcon,
+    ATag
+  },
+  data() {
+    return {
+      current: "1"
+    };
+  },
+  computed: {
+    currUser() {
+      return this.$store.state.account.user;
     }
-}
+  },
+  methods: {
+    handleClick(e) {
+      this.current = e.key;
+    }
+  }
+};
 </script>
 
 <style lang="less">
 .title {
-    color: rgba(0,0,0,.85);
-    font-size: 16px;
-    font-weight: 500;
-    margin-bottom: 16px;
+  color: rgba(0, 0, 0, 0.85);
+  font-size: 16px;
+  font-weight: 500;
+  margin-bottom: 16px;
 }
 
 .settings .ant-card-body {
-    padding-left: 0;
+  padding-left: 0;
 }
 
 .settings .ant-menu-item-selected {
-    font-weight: 700;
+  font-weight: 700;
 }
 
 .main {
-    display: flex;
-    padding-top: 16px;
-    .col-right {
-        padding: 8px 40px;
-        width: 100%;
-        .title {
-            color: rgba(0,0,0,.85);
-            font-size: 20px;
-            font-weight: 500;
-            line-height: 28px;
-            margin-bottom: 12px;
-        }
+  display: flex;
+  padding-top: 16px;
+  .col-right {
+    padding: 8px 40px;
+    width: 100%;
+    .title {
+      color: rgba(0, 0, 0, 0.85);
+      font-size: 20px;
+      font-weight: 500;
+      line-height: 28px;
+      margin-bottom: 12px;
     }
+  }
 }
 
 .profile-content {
-    display: flex;
-    .left{
-        max-width: 448px;
-        min-width: 224px;
-        float: left;
-        text-align: left;
-        .ant-form-item {
-            margin-bottom: 15px;
-        }
-        .ant-form-item-label {
-            text-align: left;
-            line-height: 2;
-        }
+  display: flex;
+  .left {
+    max-width: 448px;
+    min-width: 224px;
+    float: left;
+    text-align: left;
+    .ant-form-item {
+      margin-bottom: 15px;
     }
-    .right {
-        padding-left: 104px;
-        position: relative;
+    .ant-form-item-label {
+      text-align: left;
+      line-height: 2;
     }
+  }
+  .right {
+    padding-left: 104px;
+    position: relative;
+    .ant-form-item-label {
+      text-align: left;
+      line-height: 2;
+    }
+  }
 }
 </style>
