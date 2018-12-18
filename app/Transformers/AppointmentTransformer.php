d03ca778d58795037630fe17f2df0d905110d7cc
sheepmanchen <11611310@mail.sustc.edu.cn>
2018-12-18 19:18:44 +0800

    appointment

--- app/Transformers/AppointmentTransformer.php
+++ app/Transformers/AppointmentTransformer.php
@@ -10,22 +10,12 @@ class AppointmentTransformer extends TransformerAbstract
     public function transform(Appointment $appointment)
     {
         
-        if ($appointment->student()){
-            return [
-                'id' => $appointment->id,
-                'sid' => $appointment->student_id,
-                'reserved_time' => $appointment->reserved_time,
-                'content' => $appointment->content,
-            ];
-        }
-        else if ($appointment->faculty()){
-            return [
-                'id' => $appointment->id,
-                'sid' => $appointment->faculty_id,
-                'reserved_time' => $appointment->reserved_time,
-                'content' => $appointment->content,
-            ];
-        }
-
+        return [
+            'id' => $appointment->id,
+            'sid' => $appointment->faculty_id,
+            'reserved_time' => $appointment->reserved_time,
+            'content' => $appointment->content,
+        ];
+        
     }
 }
