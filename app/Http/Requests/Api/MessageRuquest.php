662c665064886af8005b0aa17188292ba7276010
lujinghan <11611211@mail.sustc.edu.cn>
2018-12-21 15:09:38 +0800

    2

---
+++ app/Http/Requests/Api/MessageRuquest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Dingo\Api\Http\FormRequest;
+
+class MessageRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'receiver_id'=>'required|numeric|regex: /^[0-9]{8}$/|unique:users',
+            'content'=>'required',
+        ]
+    }
+}    
---
+++ app/Transformers/MessageTransformer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Models\Message;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    public function transform(Message $message)
+    {
+        return [
+            'id' =>$message->id,
+            'sender_id'=>$message->sender_id,
+            'receiver_id'=>$message->receiver_id,
+            'content'=>$message->content,
+            'reply_src_id'=>$message->reply_src_id,
+            'created_at' => $message->created_at->toDateTimeString(),
+            'updated_at' => $message->updated_at->toDateTimeString(),
+        ];
+    }
+}
