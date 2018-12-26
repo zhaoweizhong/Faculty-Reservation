57a6bfc82772ed89531ab8c7a1f0f583c3203f29
Zhaowei Zhong <zhong98zhaowei@gmail.com>
2018-12-26 21:01:12 +0800

    Small Changes

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\AuthorizationRequest;
 
 class AuthorizationsController extends Controller
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
-use Illuminate\Http\Request;
 use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
 
